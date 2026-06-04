c13070374d7621d2960a3bbcf829d843279007c0
Michael Linares <michaellinaresdbildungs@gmail.com>
2026-06-04 21:12:47 +0200

    Added metadata to home.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,28 @@ import Links from '@/components/Links';
 
 import Music from '@/components/Music';
 import PhotoGallery from '@/components/PhotoGallery';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: 'https://linares-press-kit.vercel.app',
+  },
+  openGraph: {
+    title: 'Linarex - Composer & Music Producer',
+    description:
+      'Discover innovative music by Linarex. Stream "Vaivén" now on Spotify, Apple Music, YouTube Music and more.',
+    url: 'https://linares-press-kit.vercel.app',
+    images: [
+      {
+        url: 'https://linares-press-kit.vercel.app/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Linarex - Composer & Music Producer',
+        type: 'image/jpeg',
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
